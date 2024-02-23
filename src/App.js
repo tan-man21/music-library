c40e7db9b69d902a031b27b81eafc7e52d800408
Tanner Rogers <trroger2@alumni.ncsu.edu>
2024-02-23 12:35:51 -0500

    added artist and album links to views

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-import {useEffect, useState, useRef} from 'react';
+import {useState, useRef, Fragment} from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import SearchBar from './components/SearchBar';
 import Gallery from './components/Gallery';
 import { DataContext } from './context/DataContext';
 import { SearchContext } from './context/SearchContext';
+import AlbumView from './components/AlbumView';
+import ArtistView from './components/ArtistView';
 
 function App() {
   let [message, setMessage] = useState('Search for Music!')
@@ -29,16 +32,26 @@ function App() {
 
   return (
     <div className="App">
-      <SearchContext.Provider value={{
-        term: searchInput,
-        handleSearch: handleSearch
-      }}>
-        <SearchBar />
-      </SearchContext.Provider>
       {message}
-      <DataContext.Provider value={data} >
-      <Gallery />
-      </DataContext.Provider>
+      <Router>
+        <Routes>
+          <Route path='/' element={
+            <Fragment>
+              <SearchContext.Provider value={{
+                term: searchInput,
+                handleSearch: handleSearch
+              }}>
+                <SearchBar />
+              </SearchContext.Provider>
+              <DataContext.Provider value={data} >
+              <Gallery />
+              </DataContext.Provider>
+            </Fragment>
+          } />
+          <Route path='/album/:id' element={<AlbumView />} />
+          <Route path='/artist/:id' element={<ArtistView />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
