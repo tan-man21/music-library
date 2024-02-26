05dcc5cd0096ebab49421dbf579b161b2ae30e59
Tanner Rogers <trroger2@alumni.ncsu.edu>
2024-02-25 19:58:05 -0500

    added detailed views to album and artist views

--- src/components/AlbumView.js
+++ src/components/AlbumView.js
@@ -1,14 +1,46 @@
 import {useState, useEffect} from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 function AlbumView() {
+    const navigate = useNavigate()
     const {id} = useParams()
-    const {albumData, setAlbumData} = useState([])
+    const [albumData, setAlbumData] = useState([])
+
+    const navButtons = () => {
+        return (
+            <div>
+                <button onClick={() => navigate(-1)}>Back</button>
+
+                <button onClick={() => navigate('/')}>Home</button>
+            </div>
+        )
+    }
+
+    useEffect (() => {
+        const API_URL = `http://localhost:4000/song/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setAlbumData(resData.results)
+        }
+        fetchData()
+    }, [id])
+
+    const justSongs = albumData.filter(entry => entry.wrapperType === 'track')
+
+    const renderSongs = justSongs.map((song, i) => {
+        return (
+            <div key={''}>
+                <p>{song.trackName}</p>
+            </div>
+        )
+    })
 
     return (
         <div>
-            <h2>The id passed was: {id}</h2>
-            <p>Album Data Goes Here!</p>
+            {albumData.length > 0 ? <h2>{albumData[0].collectionName}</h2> : <h2>Loading...</h2>}
+            {navButtons()}
+            {renderSongs}
         </div>
     )
 }
